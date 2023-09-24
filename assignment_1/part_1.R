c70f9b2523415401f0432638a8a6da986573355e
Christopher Stonell <chris_stonell@yahoo.co.uk>
2023-09-24 12:23:29 +1000

    Assignemtn 1

---
+++ assignment_1/part_1.R
@@ -0,0 +1,13 @@
+pollutantmean <- function(directory = "specdata", pollutant, id = 1:322) {
+  ## 'directory' is a character vector of length 1 indicating the location for the CSV files
+  ## 'pollutant' is a character vector of length 1 indicating the name of the pollutant for which we will calculate the mean; either "sulfate" or "nitrate".
+  ## 'id' is an integer vector indicating the monitor ID numbers to be used
+  ## Return the mean of the pollutant across all monitors listed in the 'id' vector. Ignore NA values.
+  ## Do NOT round the result.
+  
+  files <- list.files(paste("~/Documents/Coursera_Data_Science/datasciencecoursera/assignment_1/",directory, sep=""))[id]
+  readfiles <- lapply(files, read.csv)
+  data <- do.call(rbind, readfiles)
+  mean(data[,pollutant], na.rm = TRUE)
+
+}
